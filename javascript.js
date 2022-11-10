ee10a0a8f16670b9f94370e5a1cd76a04552458b
miguelvalden <miguelvalden77@gmail.com>
2022-11-10 03:17:17 +0100

    Clase 9

---
+++ javascript.js
@@ -0,0 +1,18 @@
+
+
+// reduce
+    // 1. Haz una función que reciba un array de números y devuelva la suba de todos ellos
+    let numbers = [2, 3, 6, 8, 10]
+    
+    
+
+    // 2. Toma la función anterior y haz que el resultado final le sean sumados otros 20.
+
+
+
+// Math.min / Math.max
+    // 3. Haz una función que reciba varios números como parámetro y devuelva el mayor.
+
+
+
+    // 4. Ahora haz que la función anterior reciba otro parámetro extra, un string que será "max" si se quiere devolver el mayor y "min" si se quiere devolver el menor
