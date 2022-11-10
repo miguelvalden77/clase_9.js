a40c29a354b110f8f665f949ab570c26d6829871
miguelvalden <miguelvalden77@gmail.com>
2022-11-10 23:15:46 +0100

    Ejercicios

--- javascript.js
+++ javascript.js
@@ -1,3 +1,32 @@
+// reduce 
+let numeros = [3, 6, 4, 5, 11, 5]
+let strings = ["Hola", "que", "tal"]
+
+let sumaArr = numeros.reduce((acc, value)=>{
+    return acc + value
+}, 6)
+
+console.log(sumaArr)
+
+let sumChar = strings.reduce((acc, value)=>{
+    return acc + value.length
+}, 0)
+
+console.log(sumChar)
+
+
+// Math.min / Math.max
+
+console.log(Math.max(2, 6, 4, 99))
+console.log(Math.min(2, 6, 4, 99))
+
+// ... son parámetros infinitos
+const maxNum = (...nums)=>{
+    return Math.max(...nums)
+}
+
+console.log(maxNum(7, 4, 1))
+
 
 
 // reduce
@@ -11,8 +40,11 @@
 
 
 // Math.min / Math.max
-    // 3. Haz una función que reciba varios números como parámetro y devuelva el mayor.
+    // 3. Haz una función que reciba varios números como parámetro y devuelva el menor.
 
 
 
-    // 4. Ahora haz que la función anterior reciba otro parámetro extra, un string que será "max" si se quiere devolver el mayor y "min" si se quiere devolver el menor
+    // 4. Ahora haz que la función anterior reciba otro parámetro extra, un string que será "max" si se quiere devolver el mayor y "min" si se quiere devolver el menor.
+
+
+    
